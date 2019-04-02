f0810c8412998bee52f981d6b152f7f3e2a00dd3
Ho-Youn Son <hoyoun.son@gmail.com>
2019-04-02 14:38:46 +0900

    Update test.py

--- 10_Chatbot/test.py
+++ 10_Chatbot/test.py
@@ -1,6 +1,6 @@
 import telegram
 
-chii_token = '815476232:AAEq1w39JJrK7OkoUOaYgyDI6Eu_OJsJM8Y'
+chii_token = '815476232:AAEq1w39JJrK7O'
 
 chii = telegram.Bot(token = chii_token)
 updates = chii .getUpdates()
